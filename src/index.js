8a2c4ee7357a28698423843268ff492dd1f56277
MaxKuryez <maxkuryez@gmail.com>
2022-04-03 17:05:51 +0200

    Added footer + header fixes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Gnav from './components/gnav/gnav_menu';
 import HomePage from './components/homepage/homepage.js';
+import Footer from './components/footer/footer.js';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <Gnav />
     <HomePage/>
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
