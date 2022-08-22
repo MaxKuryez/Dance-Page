72147759fbecea484cccac67d47a98c9caafde7d
MaxKuryez <maxkuryez@gmail.com>
2022-08-22 20:55:24 +0200

    Added language switcher

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,22 @@
 import './footer.scss';
 import vkLogo from '../../assets/global/vk-logo.png';
-import {contentHeader} from '../globals/content.js'
+import {contentHeader} from '../globals/content.js';
+import Cookies from 'universal-cookie';
 
 function Gnav() {
+
+  function handleLanguageSwitch(e, lang) {
+    e.preventDefault();
+    console.log(lang);
+    const cookies = new Cookies();
+    let currentCookie = cookies.get('Lang');
+
+    if (currentCookie != lang) {
+      cookies.set('Lang', lang, { path: '/' });
+      window.location.reload(false);
+    }
+    console.log('Current cookie: ' + cookies.get('Lang'));
+  }
 
   return (
     <div className='footer-container'>
@@ -13,8 +27,8 @@ function Gnav() {
         <div className='menu-link'>
           <span>{contentHeader.email}</span>
         </div>
-        <div className='menu-link'>
-          <span><a href='/'>РУССКИЙ</a> / <a href='/'>ENGLISH</a></span>
+        <div className='menu-link language'>
+          <span><a onClick={(e) => {handleLanguageSwitch(e, 'ru')}}>РУССКИЙ</a> / <a onClick={(e) => {handleLanguageSwitch(e, 'en')}}>ENGLISH</a></span>
         </div>
         <div className='logo-container'>
             <a href='https://vk.com/yuliyakuryezstudio' target='blank'>
--- src/components/globals/content.js
+++ src/components/globals/content.js
@@ -1,3 +1,8 @@
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+let currentCookie = cookies.get('Lang') || 'ru';
+
 const aboutUsContent = {
   header: 'Обо мне',
   subHeader: ['Мы в YULIA KURYEZ Ballroom Dresses', <br/>, 'уже 8 лет шьём бальные платья для соревнований'],
@@ -7,13 +12,15 @@ const aboutUsContent = {
     'Время шло, сын занимался в клубе и забирала я его с тренировок уже с младшей дочерью… Надо ли говорить, ',
     'что не прошо и года, как в танцах у меня уже было двое детей! Да, конечно, первое платье для дочери было ',
     'приобретено в магазине, но в какой-то момент, после первых же соревнваний, захотелось сшить рейтинговое платье ',
-    'самой – нужного фасона и цвета.'],
+    'самой – нужного фасона и цвета.'
+  ],
   banner2: ['Платье получилось – оно было ярким, подходило по цветотипу и фактуре, дочь сияла и тренеры были доволны. ',
     'Вот и вся предыстория, расказанная максимально емко.', <br/>, <br/>,
     'Мои дети, оттанцевав 10 лет, пошли другими путями в жизни, ',
     'а я занимаюсь дизайном и пошивом бальных платьев и резвычайно рада этому обстоятелству! В декабре 2019 года я ',
     'завершила второе всшее образование и стала дипломированным специалистом – Стилист-имиджмейкер широкого профиля. ',
-    'Конечно, курс чрезвычайно помогает мне а работе.'],
+    'Конечно, курс чрезвычайно помогает мне а работе.'
+  ],
 }
 
 const homepageContent = {
@@ -22,11 +29,13 @@ const homepageContent = {
   banner1: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
     'ut labore et dolore magna aliqua. Etiam sit amet nisl purus in mollis.', <br/>, <br/>, 
     'Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Augue ut lectus arcu bibendum at varius.',
-    'Dolor sed viverra ipsum nunc.'],
+    'Dolor sed viverra ipsum nunc.'
+  ],
   banner2: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
     'ut labore et dolore magna aliqua. Etiam sit amet nisl purus in mollis.', <br/>, <br/>, 
     'Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Augue ut lectus arcu bibendum at varius.',
-    'Dolor sed viverra ipsum nunc.'],
+    'Dolor sed viverra ipsum nunc.'
+  ],
   header2: 'Платья, выпущенные на паркет',
   priceHeader: 'Цены.',
   priceList: {'list' : [
@@ -57,21 +66,39 @@ const homepageContent = {
   ]}
 }
 
-const contactUsContent = {
-  header: 'Сконтактируйтесь со мной',
-  thanks: 'Спасибо за ваш вопрос!',
-  name: 'Имя',
-  surname: 'Фамилия',
-  email: 'Электронная Почта',
-  question: 'Вопрос',
-  phone: 'Телефон',
-  nameValidation: 'Это поле является обязательным!',
-  surnameValidation: 'Это поле является обязательным!',
-  emailValidation: 'Пожалуйста, введите почту в формате email@example.com.',
-  questionValidation: 'Это поле является обязательным!',
-  phoneValidation: 'Пожалуйста, введите телефон в формате 123456789.',
-  submit: 'Задать вопрос',
-  generalError: 'Что-то пошло не так.',
+const contactUsContentLang = {
+  ru: {
+    header: 'Сконтактируйтесь Со Мной',
+    thanks: 'Спасибо за ваш вопрос!',
+    name: 'Имя',
+    surname: 'Фамилия',
+    email: 'Электронная Почта',
+    question: 'Вопрос',
+    phone: 'Телефон',
+    nameValidation: 'Это поле является обязательным!',
+    surnameValidation: 'Это поле является обязательным!',
+    emailValidation: 'Пожалуйста, введите почту в формате email@example.com.',
+    questionValidation: 'Это поле является обязательным!',
+    phoneValidation: 'Пожалуйста, введите телефон в формате 123456789.',
+    submit: 'Задать вопрос',
+    generalError: 'Что-то пошло не так.',
+  },
+  en: {
+    header: 'Contact Me',
+    thanks: 'Thank you for your question!',
+    name: 'Name',
+    surname: 'Surname',
+    email: 'Email',
+    question: 'Question',
+    phone: 'Phone',
+    nameValidation: 'This field is required!',
+    surnameValidation: 'This field is required!',
+    emailValidation: 'Please, enter the email in such format: email@example.com.',
+    questionValidation: 'This field is required!',
+    phoneValidation: 'Please, enter the phone in such format: 123456789.',
+    submit: 'Submit',
+    generalError: 'Something went wrong!',
+  },
 }
 
 const catalogueContent = {
@@ -96,20 +123,44 @@ const catalogueContent = {
   ],
 }
 
-const notFound = {
-  header: ['Страница не найдена', <h1>404</h1>],
+const notFoundLang = {
+  ru: {
+    header: ['Страница не найдена', <h1>404</h1>],
+  },
+  en: {
+    header: ['Page not found', <h1>404</h1>],
+  },
 }
 
-const contentGnav = {
-  aboutMe: 'ОБО МНЕ',
-  catalogue: 'МОИ РАБОТЫ',
-  process: 'ПРОЦЕСС',
-  contactMe: 'КОНТАКТ',
+const contentGnavLang = {
+  ru: {
+    aboutMe: 'ОБО МНЕ',
+    catalogue: 'МОИ РАБОТЫ',
+    process: 'ПРОЦЕСС',
+    contactMe: 'КОНТАКТ',
+  },
+  en: {
+    aboutMe: 'ABOUT ME',
+    catalogue: 'MY WORK',
+    process: 'PROCESS',
+    contactMe: 'CONTACT ME',
+  },
 }
 
-const contentHeader = {
-  email: ['Почта: ', <a href='mailto:test@test.mail'>test@test.mail</a>],
-  phone: ['Телефон: ', <a href='tel:+71235678'>+7 123 56 78</a>],
+const contentHeaderLang = {
+  ru: {
+    email: ['Почта: ', <a href='mailto:test@test.mail'>test@test.mail</a>],
+    phone: ['Телефон: ', <a href='tel:+71235678'>+7 123 56 78</a>],
+  },
+  en: {
+    email: ['Email: ', <a href='mailto:test@test.mail'>test@test.mail</a>],
+    phone: ['Phone: ', <a href='tel:+71235678'>+7 123 56 78</a>],
+  },
 }
+
+const contentGnav = contentGnavLang[currentCookie];
+const contentHeader = contentHeaderLang[currentCookie];
+const contactUsContent = contactUsContentLang[currentCookie];
+const notFound = notFoundLang[currentCookie];
 
 export {aboutUsContent, homepageContent, contactUsContent, catalogueContent, notFound, contentGnav, contentHeader};
